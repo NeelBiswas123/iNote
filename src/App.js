def539d1b083d4259b40ecee5bac7aa488b0ce17
Your Name <you@example.com>
2025-05-03 10:00:16 +0530

    d56-working on frontend make the about, home page on react dom........also update package.json for concurrently run back and frontend together see line 28

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <Router>
+
+      <Navbar />
+    
+      <Routes>
+          {/* <Route path="/about">
+            <About />
+          </Route>  old method doesnot work*/ }
+
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+         
+        </Routes>
+    </Router>
+    </>
   )
 }
